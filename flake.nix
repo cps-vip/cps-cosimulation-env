761d684ec83e327057d6f00464422b9ffb0ad63d
Will Stuckey <guyfleeman@gmail.com>
2023-08-04 16:50:26 -0400

    move HELICS commit forward to ignore build files

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "HELICS";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux" ]
+    (system: 
+      let 
+        pkgs = import nixpkgs {
+          inherit system; 
+        };
+
+        packageName = "vip-helics";
+
+      in {
+        devShell = pkgs.mkShell {
+
+          shellHook = ''
+            export PATH=$(realpath ./install/bin):$PATH
+            export REPO_ROOT=$(realpath ./.)
+          '';
+
+          buildInputs = with pkgs; [
+            # C/C++ build utils
+            gnumake
+            cmake
+
+            boost
+            czmq
+            ncurses
+          ];
+        };
+      }
+    );
+}
