06cd08220a3bddecdbfb03e3ed33bc68ad63b6a2
Will Stuckey <guyfleeman@gmail.com>
2023-09-15 19:03:23 -0400

    right way to do things, but broken

--- flake.nix
+++ flake.nix
@@ -4,24 +4,58 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    {
+      overlay = nixpkgs.lib.composeManyExtensions [
+        poetry2nix.overlay
+          (final: prev: {
+            # we may want this eventually
+            # myapp = prev.poetry2nix.mkPoetryApplication {
+            #   projectDir = ./.;
+            # };
+
+            # seutp a python environment
+            myenv = prev.poetry2nix.mkPoetryEnv {
+              projectDir = ./.;
+            };
+          })
+        ];
+    } // (flake-utils.lib.eachSystem [
+    # flake-utils.lib.eachSystem [
         "aarch64-linux"
         "aarch64-darwin"
         "x86_64-darwin"
         "x86_64-linux" ]
     (system: 
       let 
+        pybootstrap_pkgs = import nixpkgs {
+          inherit system;
+        };
+
         pkgs = import nixpkgs {
           inherit system; 
+          overlays = [ self.overlay ];
         };
 
         packageName = "vip-cosim";
 
       in {
-        devShell = pkgs.mkShell {
+        packages.myenv = pkgs.myenv;
+
+        devShells.poetry = pybootstrap_pkgs.mkShell {
+          buildInputs = with pybootstrap_pkgs; [
+            poetry
+            python3
+          ];
+        };
+
+        devShells.default = pkgs.mkShell {
 
           shellHook = ''
             export PATH=$(realpath ./install/bin):$PATH
@@ -39,8 +73,10 @@
             boost
             czmq
             ncurses
+
+            myenv
           ];
         };
       }
-    );
+    ));
 }
