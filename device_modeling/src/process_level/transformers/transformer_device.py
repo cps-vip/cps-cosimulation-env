0eeca77f5839f7f71315d1cc7a5138969dcf643c
connormccary <connor.mccary27@gmail.com>
2023-11-03 09:02:14 -0400

    Fixed initial errors in transformer class
    Made variables names consistent and defined variables not defined in init constructor but used in other methods.

--- device_modeling/src/process_level/transformers/transformer_device.py
+++ device_modeling/src/process_level/transformers/transformer_device.py
@@ -1,26 +1,57 @@
 class Transformer:
     def __init__(self, name: str, communication_protocol: str, transformer_type: str):
+        self.name = name
+        self.communication_protocol = communication_protocol
         self.transformer_type = transformer_type
         self.primary_voltage = 0
         self.secondary_voltage = 0
+        self.tap = 0
+        self.taps = []
 
     #Getters/Setters
     
     def set_primary_voltage(self, voltage):
-        self.input_voltage = 0
+        self.input_voltage = voltage
     
     def get_primary_voltage(self, voltage):
         return self.input_voltage
     
+    
+
     def set_secondary_voltage(self, voltage):
-        self.output_voltage = 0
+        self.output_voltage = voltage
     
     def get_secondary_voltage(self, voltage):
         return self.output_voltage
     
-    def set_state(self, primary_volatage, secondary_voltage):
+
+
+    def set_tap(self, position):
+        if 0 <= position < len(self.taps):
+            self.tap_position = position
+        else:
+            print("Invalid tap position. Tap position remains unchanged.")
+
+    def get_tap(self):
+        return self.tap
+
+
+
+    def set_taps(self, num_taps):
+        self.taps = list(range(num_taps))
+
+    def get_taps(self):
+        return self.taps
+    
+
+
+    def set_state(self, primary_volatage, secondary_voltage, tap, num_taps):
         self.set_primary_voltage(primary_volatage)
         self.set_secondary_voltage(secondary_voltage)
+        self.set_tap(tap)
+        self.set_taps(num_taps)
+
+        
 
     def get_transformer_type(self) -> str:
         return self.transformer_type
@@ -32,16 +63,10 @@ class Transformer:
         return self.primary_voltage * self.get_turns_ratio()
     
     
-    #Behavior
-    def set_tap(self, position):
-        if 0 <= position and position < len(self.taps):
-            self.tap_position = position
-        else:
-            print("Invalid tap position. Tap position remains unchanged.")
-
+    
     def adjust_voltage(self, adjustment):
         new_tap_position = self.tap_position + adjustment
-        if 0 <= new_tap_position  and new_tap_position < len(self.taps):
+        if 0 <= new_tap_position < len(self.taps):
             self.tap_position = new_tap_position
             self.output_voltage = self.primary_voltage * self.get_turns_ratio()
         else:
