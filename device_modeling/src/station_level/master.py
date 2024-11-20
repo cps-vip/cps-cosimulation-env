10ee74445e601fb796083d19d512a2905f20f26d
Adam McAndrews <adammacs1@gmail.com>
2024-11-19 21:36:19 -0500

    Set up master.py template

--- device_modeling/src/station_level/master.py
+++ device_modeling/src/station_level/master.py
@@ -1,9 +1,22 @@
-from src.station_level.station_device import StationDevice
+from station_level.station_device import StationDevice
 
-# TODO - everything
+# TODO - 
+# init
+# destroy
+# activate
+# deactivate
 class DNP3Master(StationDevice):
     def __init__(self, name: str, dnp3_addr: int, outstation_ip: str):
         super().__init__(name)
         self.dnp3_addr = dnp3_addr
         self.outstation_ip = outstation_ip
 
+    # Implement abstract method in Device base class
+    def destroy(self) -> None:
+
+    # Implement abstract method in Device base class
+    def activate(self) -> None:
+       
+    # Implement abstract method in Device base class
+    def deactivate(self) -> None:
+        
--- device_modeling/src/station_level/station_device.py
+++ device_modeling/src/station_level/station_device.py
@@ -1,5 +1,5 @@
-from dnp3.dnp3_outstation import DNP3Outstation
+from device_base.device import Device
 
 class StationDevice(Device):
-    def __init__(self, name: str, outstation_addr: int, master_addr: int, socket_addr: str):
-        super().__init__(name, outstation_addr, master_addr, socket_addr)
+    def __init__(self, name: str):
+        super().__init__(name)
