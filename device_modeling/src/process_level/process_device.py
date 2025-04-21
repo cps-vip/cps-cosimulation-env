0cb27818a98d03a90865928a5e728b0e48de78fd
Chungman Kim <126294580+Serenata27@users.noreply.github.com>
2025-04-21 13:57:45 -0400

    Final
    -Completed device modeling.
    -Updated Class diagram
    -New Diagram from DNP3 API

--- device_modeling/src/process_level/process_device.py
+++ device_modeling/src/process_level/process_device.py
@@ -3,3 +3,7 @@ from dnp3.dnp3_outstation import DNP3Outstation
 class ProcessDevice(DNP3Outstation):
     def __init__(self, name: str, outstation_addr: int, master_addr: int, socket_addr: str):
         super().__init__(name, outstation_addr, master_addr, socket_addr)
+        self.device_name = name
+
+    def get_device_name(self) -> str:
+        return self.device_name
