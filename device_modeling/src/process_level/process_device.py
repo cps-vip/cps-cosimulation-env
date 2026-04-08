b6be869c0f04dbcf860dcf5efed60ee1e375d0f3
N-Reed1 <nickereed1@gmail.com>
2026-04-08 12:42:28 -0400

    Finalized integration of RTU, Transformers, and Documentation

--- device_modeling/src/process_level/process_device.py
+++ device_modeling/src/process_level/process_device.py
@@ -1,5 +1,9 @@
-from dnp3.devices.outstation import Outstation
+from dnp3.dnp3_outstation import DNP3Outstation
 
-class ProcessDevice(Outstation):
+class ProcessDevice(DNP3Outstation):
     def __init__(self, name: str, outstation_addr: int, master_addr: int, socket_addr: str):
         super().__init__(name, outstation_addr, master_addr, socket_addr)
+        self.device_name = name
+
+    def get_device_name(self) -> str:
+        return self.device_name
--- device_modeling/src/process_level/transformers/transformer_device.py
+++ device_modeling/src/process_level/transformers/transformer_device.py
@@ -4,57 +4,6 @@ class Transformer:
     def __init__(self, name: str, transformer_type: str):
         self.name = name
         self.transformer_type = transformer_type
-        self.primary_voltage: float = 0.0 #input voltage
-        self.secondary_voltage: float = 0.0 #output voltage
-        self.tap_position: float = 1.0
 
 
-    def set_primary_voltage(self, voltage: float) -> None:
-        if voltage <= 0:
-            raise ValueError("Voltage need to set other than zero")
-        self.primary_voltage = voltage
 
-
-    def get_primary_voltage(self) -> float:
-        return self.primary_voltage
-
-
-    def set_secondary_voltage(self) -> None:
-        if self.primary_voltage == 0.0:
-            raise ValueError("Primary voltage is not set.")
-
-        if not self.taps:
-            raise ValueError("Turn ratio is not set.")
-
-        # Calculate the secondary voltage using primary voltage and current turn ratio
-        secondary_voltage = self.primary_voltage * self.get_turns_ratio()
-
-        # Set the calculated secondary voltage
-        self.secondary_voltage = secondary_voltage
-
-
-    def get_secondary_voltage(self) -> float:
-        return self.secondary_voltage
-
-    def set_tap(self, position: float) -> None:
-        self.tap_position = position
-        
-    def get_tap(self) -> int:
-        return self.tap_position
-
-    def set_state(self, primary_voltage: float, secondary_voltage: float, tap: int, num_taps: int) -> None:
-        self.set_primary_voltage(primary_voltage)
-        self.set_secondary_voltage(secondary_voltage)
-        self.set_tap(tap)
-        self.set_taps(num_taps)
-
-    def get_transformer_type(self) -> str:
-        return self.transformer_type
-
-    def get_output_voltage(self) -> float:
-        return self.get_primary_voltage()
-
-    def adjust_voltage(self, desired_voltage: float) -> None:
-        ratio = desired_voltage/self.get_primary_voltage
-        self.set_tap(ratio)
-        self.set_secondary_voltage()
